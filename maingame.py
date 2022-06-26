b3ac3a492d93a889e29d4fb433a73e843ae06bed
MBANS8A1 <62100017+MBANS8A1@users.noreply.github.com>
2022-06-26 13:01:54 +0100

    Update maingame.py
    I added some member variables (coordinates) to the Scenery and Floor classes with corresponding rendering functions. Also, I added objects for both these classes outside the game loop and called the rendering functions within the game loop.

--- maingame.py
+++ maingame.py
@@ -39,10 +39,25 @@ running = 1
 class Scenery(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
+        self.sceneryImage = pygame.image.load ("Background.png")
+        self.rect = self.image.get_rect(center=(355,480))
+        self.scX = 0
+        self.scY = 0
+        
+    def renderingS(self):
+        winDisplay.blit(self.sceneryImage, (self.scX,self.scY))
 
 class Floor(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
+        self.floorImage = pygame.image.load ("Ground.png")
+        self.rect = self.image.get_rect(center=(355,480))
+        self.floorX = 0
+        self.floorY = 0
+        
+    
+     def renderingF(self):
+        winDisplay.blit(self.floorImage, (self.floorX,self.floorY))   
 
 class Hero(pygame.sprite.Sprite):
     def __init__(self):
@@ -54,7 +69,9 @@ class Enemy(pygame.sprite.Sprite):
 
 
 
-
+#creating objects of classes
+scenery = Scenery()
+floor = Floor()
 
 
 
@@ -79,6 +96,10 @@ while running :
 
     if k_buttons[pygame.K_DOWN]:
         pass
+    
+    
+    scenery.renderingS()
+    floor.renderingF()
     
     #Whole screen needs to be updated in frame transition
     pygame.display.update()
